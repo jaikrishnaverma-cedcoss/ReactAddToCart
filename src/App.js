5fb8c3d5b71731e3c8dfb8ae26febaa9106d13b5
jaikrishnaverma <jaikrishnaverma>
2022-09-13 15:32:52 +0530

    add to cart1

--- src/App.js
+++ src/App.js
@@ -8,37 +8,43 @@ import Header from './Component/Master/Header';
 import products from './Data';
 function App() {
 
-  const [cart,setCart]=useState([])
-  const addToCart=(newData)=>{
-    let newArr={...newData,quantity:1}
-    let foundValue=-1
-    cart.map((x,i)=>{if(newData.id===x.id){foundValue=i} return ""});
-    if(foundValue===-1)
-    cart.push(newArr)
+  const [cart, setCart] = useState([])
+
+
+  const addToCart = (newData) => {
+    let newArr = { ...newData, quantity: 1 }
+    let foundValue = -1
+    cart.map((x, i) => { if (newData.id === x.id) { foundValue = i } return "" });
+    if (foundValue === -1)
+      cart.push(newArr)
     else
-    cart[foundValue].quantity+=1
+      cart[foundValue].quantity += 1
     setCart([...cart])
-    console.log(cart)
+    // console.log(cart)
 
   }
-  const empty=(index)=>{
-  setCart([])
-  }
-const remove=(index)=>{
-    cart.splice(index,1)
-    setCart([...cart])
-}
-  const incrDecr=(boolVal,index)=>{
 
-   (boolVal)?cart[index].quantity+=1:(cart[index].quantity-1===0)?cart[index].quantity=cart[index].quantity:cart[index].quantity-=1;
-    setCart([...cart])
-
+  const empty = (index) => {
+    setCart([])
   }
+
+  const remove = (index) => {
+    cart.splice(index, 1)
+    setCart([...cart])
+  }
+  const incrDecr = (boolVal, index) => {
+    (boolVal) ? cart[index].quantity += 1 : (cart[index].quantity - 1 === 0) ? cart[index].quantity = cart[index].quantity : cart[index].quantity -= 1;
+    setCart([...cart])
+  }
+
+
+
+
   return (
     <div className="App">
       <Header />
-      <Body addToCart={addToCart}/>
-      <Cart arr={cart} empty={empty} remove={remove} incrDecr={incrDecr}/>
+      <Body addToCart={addToCart} />
+      <Cart arr={cart} empty={empty} remove={remove} incrDecr={incrDecr} />
       <Footer />
     </div>
   );
